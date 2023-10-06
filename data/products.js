5a569e450c6d0564f593211de32486bd0aa56593
chaimae <chaimaer759@gmail.com>
2023-10-06 15:40:24 +0200

    use products.js file

---
+++ data/products.js
@@ -0,0 +1,51 @@
+const products=[
+    {
+        image:'./icons/athletic-cotton-socks-6-pairs.jpg',
+        name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
+        rating:{
+            stars:4.5,
+            count:87
+        },
+        priceCent:1090/*js has problems with float addition opertaions */
+    },
+    //2 nd prd
+    {
+        image:'./icons/intermediate-composite-basketball.jpg',
+        name:'Intermediate Size Basketball',
+        rating:{
+            stars:4,
+            count:127
+        },
+        priceCent:2095/*js has problems with float addition opertaions  1dollar=100cents*/
+    },
+    //3 rd prd
+    {
+        image:'./icons/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+        name:'Adults Plain Cotton T-Shirt - 2 Pack',
+        rating:{
+            stars:4.5,
+            count:56
+        },
+        priceCent:799/*js has problems with float addition opertaions */
+    },
+    //4 th prd
+    {
+        image:'./icons/black-2-slot-toaster.jpg',
+        name:' 2 Slot Toaster - Black',
+        rating:{
+            stars:5,
+            count:2197
+        },
+        priceCent:1899/*js has problems with float addition opertaions */
+    },
+    //5 th prd
+    {
+        image:'./icons/6-piece-white-dinner-plate-set.jpg',
+        name:'6 Piece White Dinner Plate Set',
+        rating:{
+            stars:4,
+            count:37
+        },
+        priceCent:2067/*js has problems with float addition opertaions */
+    }
+];
