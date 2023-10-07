2fca5e0df27856a87b6ed6926b16aa884b4226a5
chaimae <chaimaer759@gmail.com>
2023-10-07 05:21:03 +0200

    make add to cart btn interactive

--- data/products.js
+++ data/products.js
@@ -1,5 +1,6 @@
 const products=[
     {
+        id:"1",
         image:'./icons/athletic-cotton-socks-6-pairs.jpg',
         name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
         rating:{
@@ -10,6 +11,7 @@ const products=[
     },
     //2 nd prd
     {
+        id:"2",
         image:'./icons/intermediate-composite-basketball.jpg',
         name:'Intermediate Size Basketball',
         rating:{
@@ -20,6 +22,7 @@ const products=[
     },
     //3 rd prd
     {
+        id:"3",
         image:'./icons/adults-plain-cotton-tshirt-2-pack-teal.jpg',
         name:'Adults Plain Cotton T-Shirt - 2 Pack',
         rating:{
@@ -30,6 +33,7 @@ const products=[
     },
     //4 th prd
     {
+        id:"4",
         image:'./icons/black-2-slot-toaster.jpg',
         name:' 2 Slot Toaster - Black',
         rating:{
@@ -40,6 +44,7 @@ const products=[
     },
     //5 th prd
     {
+        id:"5",
         image:'./icons/6-piece-white-dinner-plate-set.jpg',
         name:'6 Piece White Dinner Plate Set',
         rating:{
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -17,9 +17,9 @@ products.forEach((product,index)=>{
                 </div>
                     <div class="rating-count">${product.rating.count}</div>
                 </div>
-                <div class="product-cost">${(product.priceCent/100).toFixed(2)}</div>
+                <div class="product-cost">${(product.priceCent/100).toFixed(2 )}</div>
                 <select>
-                    <option value="1">1</option>
+                    <option value="1">1</option> 
                     <option value="2">2</option>
                     <option value="3">3</option>
                     <option value="4">4</option>
@@ -31,11 +31,38 @@ products.forEach((product,index)=>{
                     <option value="10">10</option>
                 </select>        
                 <!-- add to cart -->
-                <button class="add-to-cart-button">
+                <button class="add-to-cart-button  js-add-to-cart-button"
+                        data-product-id="${product.id}">
                     Add to Cart
                 </button>
             </div>    
         </div>
     </div> `;
     
+});
+
+document.querySelectorAll('.js-add-to-cart-button')
+  .forEach((add_to_cart_button,index)=>{
+    add_to_cart_button.addEventListener('click',()=>{
+       let matchingItem;
+       const productId=add_to_cart_button.dataset.productId;
+      
+       cart.forEach((item)=>{
+        if(productId===item.productId){
+            matchingItem=item;
+        }
+       });
+
+       if(matchingItem){
+            // element already exist in the cart =>just increase his qte
+            matchingItem.quantity++;
+       }
+       else{
+            // element does not exist in the cart =>create a new item(ibject) in the cart list 
+            cart.push({productId:productId,
+                        quantity:1
+                      });
+       }
+       console.log(cart);
+    });
 });
